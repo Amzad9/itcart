e5b4a84819986acf9d872053c6f2f3d71bec70a3
amzad9 <amzad.fineart@gmail.com>
2021-11-13 17:24:06 +0530

    six card manage ui

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import './App.scss';
 import Navigation from './components/Nav/Nav';
-import Header from './components/Header/Header';
-import Client from './components/Client/Client';
-import Video from './components/Video/Video';
-import Service from './components/Service/Service';
-import Solution from './components/Solution/Solution';
-import Business from './components/Business/Business';
-import PeopleSay from './components/PeopleSay/PeopleSay';
-import Team from './components/Team/Team'
-import ContactUs from './components/ContactUs/ContactUs';
-import NewsLetter from './components/NewsLetter/NewsLetter'
+import LandingPage from './LandingPage'
 import Footer from './components/Footer/Footer';
-import Comparison from './components/Comparison/Comparison';
 // import { BrowserRouter as Router, Switch , Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
        <Navigation />
-       <Header />
-       <Client />
-       <Video />
-       <Service />
-       <Solution />
-       <Business />
-       <PeopleSay />
-       <Comparison />
-       <Team />
-       <ContactUs />
-       <NewsLetter />
+       <LandingPage />
        <Footer />
       {/* <Router>
         
